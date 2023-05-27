c8a0a7ae6e0e3897825809006c551e4b938f5701
texnoman <texnoman1@yandex.ru>
2023-05-27 16:01:54 +0300

    hw2_add_material

---
+++ ДЗ_2/My-business-card-main/My-business-card-main/README.md
@@ -0,0 +1,2 @@
+# My first website
+![image](https://user-images.githubusercontent.com/99810114/192766928-3cc30540-dac4-440c-b42d-a344014cce77.png)
---
+++ ДЗ_2/My-business-card-main/My-business-card-main/js/username.js
@@ -0,0 +1,6 @@
+function askName() {
+    let name = prompt("Привет, как тебя зовут?", "введи здесь имя");
+    alert(`Приятного пользования, ${name}!`);
+}
+
+askName()
